601af615b9ce0a6849b5165c6e4fbe612035a175
phil-veeqo <phil@veeqo.com>
2018-07-23 14:58:53 +0100

    update pagination css

--- index.php
+++ index.php
@@ -166,7 +166,7 @@
                                     </input>
                                 </li>-->
                                 <?php for ($i=1; $i<=$page_count; $i++): ?>
-                                    <li class="page-item btn <?php if ($i==$page) echo("active btn-primary veeqo-background"); ?>">
+                                    <li class="<?php if ($i==$page) echo("active"); ?>">
                                         <input type="submit" name="page" class="page-link" value="<?php echo($i) ?>" />
                                     </li>
                                 <?php endfor; ?>
