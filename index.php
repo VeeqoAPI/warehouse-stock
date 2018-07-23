2a6c4cffcf17b0293535675f3d034d5411babb85
phil-veeqo <phil@veeqo.com>
2018-07-23 15:09:17 +0100

    upgrade to bootstrap 4

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 
     <!-- Bootstrap Core CSS -->
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
 
     <!-- Theme CSS -->
     <link href="main.css" rel="stylesheet">
@@ -161,9 +161,7 @@
                             <ul class="pagination justify-content-center">
                                 <?php for ($i=1; $i<=$page_count; $i++): ?>
                                     <li class="<?php if ($i==$page) echo("active"); ?>">
-                                        <a>
-                                            <input type="submit" name="page" class="page-link" value="<?php echo($i) ?>" />
-                                        </a>
+                                        <input type="submit" name="page" class="page-link" value="<?php echo($i) ?>" />
                                     </li>
                                 <?php endfor; ?>
                             </ul>
@@ -259,9 +257,8 @@
 </div>
 <!-- /.container -->
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
-        integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
-        crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
 </body>
 </html>
